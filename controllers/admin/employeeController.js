5934abdfa872f2baa5362ffe09dce0d5ae1eadf6
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-08-06 19:36:32 +0530

    lo

--- controllers/admin/employeeController.js
+++ controllers/admin/employeeController.js
@@ -45,6 +45,207 @@ const generateRandomPassword = (length = 12) => {
   return crypto.randomBytes(length).toString("base64").slice(0, length);
 };
 
+// exports.employeesInsert = async (req, res, next) => {
+//   try {
+//     const id = req.params.id || "";
+//     const insert = { ...req.body };
+//     const companyId = req.query?.company_id || insert.company_id;
+//     let plainPassword = insert.password;
+//     let originalEmail = "";
+//     let originalPasswordHash = "";
+
+//     if (!companyId) {
+//       return res
+//         .status(200)
+//         .send({ status: false, message: "Company ID is required" });
+//     }
+
+//     // Fetch the company record
+//     const companyData = await sqlModel.select("company", {}, { id: companyId });
+
+//     if (companyData.error || companyData.length === 0) {
+//       return res
+//         .status(200)
+//         .send({ status: false, message: "Company not found" });
+//     }
+
+//     const company = companyData[0];
+
+//     if (!id) {
+//       const maxEmployees = parseInt(company.no_of_emp, 10);
+
+//       // Count of emp
+//       const employeeCountData = await sqlModel.select(
+//         "employees",
+//         ["COUNT(*) as count"],
+//         { company_id: companyId }
+//       );
+//       if (employeeCountData.error) {
+//         return res.status(200).send(employeeCountData);
+//       }
+
+//       const currentEmployeeCount = parseInt(employeeCountData[0].count, 10);
+
+//       if (currentEmployeeCount >= maxEmployees) {
+//         return res.status(200).send({
+//           status: false,
+//           message: "Employee limit reached for this company",
+//         });
+//       }
+//     }
+
+//     if (req.files) {
+//       if (req.files.image) {
+//         insert.image = req.fileFullPath.find((path) => path.includes("image"));
+//       }
+//     }
+
+//     if (!insert.password) {
+//       plainPassword = generateRandomPassword();
+//       insert.password = plainPassword;
+//     }
+
+//     insert.password = await bcrypt.hash(insert.password, saltRounds);
+
+//     if (id) {
+//       // Fetch the original email and password hash
+//       const employeeRecord = await sqlModel.select(
+//         "employees",
+//         ["email", "password"],
+//         { id }
+//       );
+
+//       if (employeeRecord.error || employeeRecord.length === 0) {
+//         return res
+//           .status(200)
+//           .send({ status: false, message: "Employee not found" });
+//       }
+
+//       originalEmail = employeeRecord[0].email;
+//       originalPasswordHash = employeeRecord[0].password;
+
+//       if (req.body.email) {
+//         const existingemployeesWithEmail = await sqlModel.select(
+//           "employees",
+//           ["id"],
+//           { email: req.body.email }
+//         );
+//         if (
+//           existingemployeesWithEmail.length > 0 &&
+//           existingemployeesWithEmail[0].id != id
+//         ) {
+//           return res
+//             .status(200)
+//             .send({ status: false, message: "Email already exists" });
+//         }
+//       }
+
+//       if (req.body.employee_id) {
+//         const existingemployeesWithId = await sqlModel.select(
+//           "employees",
+//           ["id"],
+//           { employee_id: req.body.employee_id }
+//         );
+//         if (
+//           existingemployeesWithId.length > 0 &&
+//           existingemployeesWithId[0].id != id
+//         ) {
+//           return res
+//             .status(200)
+//             .send({ status: false, message: "Employee id already exists" });
+//         }
+//       }
+
+//       if (req.fileFullPath && req.fileFullPath.length > 0) {
+//         const employeesRecord = await sqlModel.select("employees", ["image"], {
+//           id,
+//         });
+
+//         if (employeesRecord.error || employeesRecord.length === 0) {
+//           return res
+//             .status(200)
+//             .send({ status: false, message: "Employee not found" });
+//         }
+
+//         const { image: oldImagePath } = employeesRecord[0];
+
+//         if (insert.image && oldImagePath) {
+//           deleteOldFile.deleteOldFile(oldImagePath);
+//         }
+//       }
+
+//       insert.updated_at = getCurrentDateTime();
+//       const saveData = await sqlModel.update("employees", insert, { id });
+//       const msg = "Data Updated";
+
+//       if (saveData.error) {
+//         return res.status(200).send(saveData);
+//       } else {
+//         res.status(200).send({ status: true, message: msg });
+//       }
+
+//       // Check if email or password has changed and send email
+//       if (req.body.email || plainPassword) {
+//         if (
+//           originalEmail !== req.body.email ||
+//           !(await bcrypt.compare(plainPassword, originalPasswordHash))
+//         ) {
+//           const emailData = {
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: plainPassword,
+//           };
+
+//           sendMail.sendEmailToEmp(emailData);
+//         }
+//       }
+//     } else {
+//       const existingemployeesWithEmail = await sqlModel.select(
+//         "employees",
+//         ["id"],
+//         { email: req.body.email }
+//       );
+//       if (existingemployeesWithEmail.length > 0) {
+//         return res
+//           .status(200)
+//           .send({ status: false, message: "Email already exists" });
+//       }
+
+//       const existingemployeesWithId = await sqlModel.select(
+//         "employees",
+//         ["id"],
+//         { employee_id: req.body.employee_id }
+//       );
+//       if (existingemployeesWithId.length > 0) {
+//         return res
+//           .status(200)
+//           .send({ status: false, message: "Employee id already exists" });
+//       }
+
+//       insert.created_at = getCurrentDateTime();
+//       const saveData = await sqlModel.insert("employees", insert);
+
+//       const emailData = {
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: plainPassword,
+//       };
+
+//       sendMail.sendEmailToEmp(emailData);
+
+//       const msg = "Data Saved";
+
+//       if (saveData.error) {
+//         return res.status(200).send(saveData);
+//       } else {
+//         return res.status(200).send({ status: true, message: msg });
+//       }
+//     }
+//   } catch (error) {
+//     return res.status(200).send({ status: false, error: error.message });
+//   }
+// };
+
 exports.employeesInsert = async (req, res, next) => {
   try {
     const id = req.params.id || "";
@@ -70,25 +271,30 @@ exports.employeesInsert = async (req, res, next) => {
     }
 
     const company = companyData[0];
-    const maxEmployees = parseInt(company.no_of_emp, 10);
 
-    // Count of emp
-    const employeeCountData = await sqlModel.select(
-      "employees",
-      ["COUNT(*) as count"],
-      { company_id: companyId }
-    );
-    if (employeeCountData.error) {
-      return res.status(200).send(employeeCountData);
-    }
+    if (!id) {
+      const maxEmployees = parseInt(company.no_of_emp, 10);
 
-    const currentEmployeeCount = parseInt(employeeCountData[0].count, 10);
+      // Count of emp
+      const employeeCountData = await sqlModel.select(
+        "employees",
+        ["COUNT(*) as count"],
+        { company_id: companyId }
+      );
+      if (employeeCountData.error) {
+        return res.status(200).send(employeeCountData);
+      }
 
-    if (currentEmployeeCount >= maxEmployees) {
-      return res.status(200).send({
-        status: false,
-        message: "Employee limit reached for this company",
-      });
+      const currentEmployeeCount = parseInt(employeeCountData[0].count, 10);
+
+      if (currentEmployeeCount >= maxEmployees) {
+        return res
+          .status(200)
+          .send({
+            status: false,
+            message: "Employee limit reached for this company",
+          });
+      }
     }
 
     if (req.files) {
@@ -97,12 +303,13 @@ exports.employeesInsert = async (req, res, next) => {
       }
     }
 
-    if (!insert.password) {
-      plainPassword = generateRandomPassword();
-      insert.password = plainPassword;
+    // Only process password if it is provided
+    if (insert.password) {
+      insert.password = await bcrypt.hash(insert.password, saltRounds);
+    } else {
+      // Remove password from insert if it's not provided
+      delete insert.password;
     }
-
-    insert.password = await bcrypt.hash(insert.password, saltRounds);
 
     if (id) {
       // Fetch the original email and password hash
@@ -172,6 +379,8 @@ exports.employeesInsert = async (req, res, next) => {
       }
 
       insert.updated_at = getCurrentDateTime();
+
+      // Update data, excluding password if not provided
       const saveData = await sqlModel.update("employees", insert, { id });
       const msg = "Data Updated";
 
@@ -185,12 +394,13 @@ exports.employeesInsert = async (req, res, next) => {
       if (req.body.email || plainPassword) {
         if (
           originalEmail !== req.body.email ||
-          !(await bcrypt.compare(plainPassword, originalPasswordHash))
+          (plainPassword &&
+            !(await bcrypt.compare(plainPassword, originalPasswordHash)))
         ) {
           const emailData = {
             name: req.body.name,
             email: req.body.email,
-            password: plainPassword,
+            password: plainPassword || originalPasswordHash, // Use existing password if not provided
           };
 
           sendMail.sendEmailToEmp(emailData);
