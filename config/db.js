e537f152dd321b1d578db7a3466a2aad809c9a67
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-08-07 12:23:46 +0530

    db

---
+++ config/db.js
@@ -0,0 +1,118 @@
+const mysql = require("mysql2");
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "upendra#0309@",
+  database: "location_tracker",
+  // password: "niket@123",
+  // database: "location_tracker",
+  connectionLimit: 10,
+});
+
+const promisePool = pool.promise();
+
+const sqlModel = {
+  execute: async (sql, values) => {
+    try {
+      const [rows, fields] = await promisePool.execute(sql, values);
+      return rows;
+    } catch (error) {
+      const errorResponse = {
+        status: false,
+        error: {
+          code: error.code || "INTERNAL_SERVER_ERROR",
+          message:
+            error.sqlMessage ||
+            "An error occurred while processing the request.",
+        },
+      };
+      throw new Error(errorResponse.error.message);
+    }
+  },
+
+  insert: async (table, data) => {
+    const keys = Object.keys(data);
+    const values = Object.values(data);
+    const placeholders = values.map(() => "?").join(", ");
+    const sql = `INSERT INTO ${table} (${keys.join(
+      ", "
+    )}) VALUES (${placeholders})`;
+    try {
+      const result = await sqlModel.execute(sql, values);
+      const lastInsertId = result.insertId;
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+  update: async (table, setValues, whereConditions = {}) => {
+    const setClause = Object.keys(setValues)
+      .map((key) => `${key} = ?`)
+      .join(", ");
+
+    const whereClause = Object.keys(whereConditions)
+      .map((key) => `${key} = ?`)
+      .join(" AND ");
+
+    const sql = `UPDATE ${table} SET ${setClause} WHERE ${whereClause}`;
+    const values = [
+      ...Object.values(setValues),
+      ...Object.values(whereConditions),
+    ];
+
+    try {
+      const result = await sqlModel.execute(sql, values);
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  delete: async (table, conditions) => {
+    const conditionKeys = Object.keys(conditions);
+    const conditionValues = conditionKeys.map((key) => conditions[key]);
+    const whereClause =
+      conditionKeys.length > 0
+        ? " WHERE " + conditionKeys.map((key) => `${key} = ?`).join(" AND ")
+        : "";
+    const sql = `DELETE FROM ${table} ${whereClause}`;
+
+    try {
+      const result = await sqlModel.execute(sql, conditionValues);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  },
+  select: async (table, columns, conditions = {}, string = "") => {
+    const columnsString = Array.isArray(columns) ? columns.join(", ") : "*";
+
+    const conditionKeys = Object.keys(conditions);
+    const conditionValues = conditionKeys.map((key) => conditions[key]);
+
+    const whereClause =
+      conditionKeys.length > 0
+        ? " WHERE " + conditionKeys.map((key) => `${key} = ?`).join(" AND ")
+        : "";
+
+    const sql = `SELECT ${columnsString} FROM ${table}${whereClause} ${string}`;
+    try {
+      const rows = await sqlModel.execute(sql, conditionValues);
+      return rows ? rows : [];
+    } catch (error) {
+      throw error;
+    }
+  },
+  customQuery: async (sql, values) => {
+    try {
+      const rows = await sqlModel.execute(sql, values);
+
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+module.exports = sqlModel;
