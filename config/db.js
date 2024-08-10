31cfad97842f7d4e04a77b8d8d4b87f6f8616e79
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-08-10 18:58:05 +0530

    check

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  // password: "upendra#0309@",
-  // database: "location_tracker",
-  password: "niket@123",
+  password: "upendra#0309@",
   database: "location_tracker",
+  // password: "niket@123",
+  // database: "location_tracker",
   connectionLimit: 10,
 });
 
