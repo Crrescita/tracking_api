e1ff652a730d5a7f506adc2f61ba7a4537134ce1
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-08-17 12:07:18 +0530

    calender

--- controllers/frontend/EmployeeController.js
+++ controllers/frontend/EmployeeController.js
@@ -186,6 +186,204 @@ exports.updateEmployee = async (req, res, next) => {
 };
 
 // emp attendance
+// exports.getEmployeeAttendance = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1];
+
+//     if (!token) {
+//       return res
+//         .status(400)
+//         .send({ status: false, message: "Token is required" });
+//     }
+
+//     const [employee] = await sqlModel.select(
+//       "employees",
+//       ["id", "company_id"],
+//       { api_token: token }
+//     );
+
+//     if (!employee) {
+//       return res
+//         .status(404)
+//         .send({ status: false, message: "Employee not found" });
+//     }
+
+//     const emp_id = employee.id;
+//     const company_id = employee.company_id;
+
+//     if (!emp_id || !company_id) {
+//       return res.status(400).send({
+//         status: false,
+//         message: "Employee ID and company ID are required",
+//       });
+//     }
+
+//     const query = `
+//       SELECT
+//         e.id,
+//         e.name,
+//         e.mobile,
+//         e.email,
+//         e.designation,
+//         e.employee_id,
+//         CASE
+//           WHEN e.image IS NOT NULL THEN CONCAT(?, e.image)
+//           ELSE e.image
+//         END AS image,
+//         c.date,
+//         c.check_in_time,
+//         c.check_out_time
+//       FROM employees e
+//       LEFT JOIN check_in c ON e.id = c.emp_id AND e.company_id = c.company_id
+//       WHERE e.id = ? AND e.company_id = ?
+//       ORDER BY c.date, c.check_in_time
+//     `;
+
+//     const values = [process.env.BASE_URL, emp_id, company_id];
+//     const data = await sqlModel.customQuery(query, values);
+
+//     if (data.error) {
+//       return res.status(500).send(data);
+//     }
+
+//     if (data.length === 0) {
+//       return res.status(200).send({ status: false, message: "No data found" });
+//     }
+
+//     const companyQuery = `
+//       SELECT
+//         check_in_time_start,
+//         check_in_time_end
+//       FROM company
+//       WHERE id = ?
+//     `;
+
+//     const companyValues = [company_id];
+//     const companyData = await sqlModel.customQuery(companyQuery, companyValues);
+
+//     const { check_in_time_start, check_in_time_end } = companyData[0] || {};
+
+//     const startDateTime = new Date(`1970-01-01T${check_in_time_start}Z`);
+//     const endDateTime = new Date(`1970-01-01T${check_in_time_end}Z`);
+
+//     const formatDuration = (totalSeconds) => {
+//       if (isNaN(totalSeconds) || totalSeconds < 0) {
+//         console.error("Invalid totalSeconds value:", totalSeconds);
+//         return "0h 0m 0s";
+//       }
+//       const hours = Math.floor(totalSeconds / 3600);
+//       const minutes = Math.floor((totalSeconds % 3600) / 60);
+//       const seconds = Math.floor(totalSeconds % 60);
+//       return `${hours}h ${minutes}m ${seconds}s`;
+//     };
+
+//     const calculateDurationInSeconds = (checkInTime, checkOutTime) => {
+//       if (!checkInTime || !checkOutTime) {
+//         return 0; // Return 0 if checkInTime or checkOutTime is null or undefined
+//       }
+//       const checkIn = new Date(`1970-01-01T${checkInTime}Z`);
+//       const checkOut = new Date(`1970-01-01T${checkOutTime}Z`);
+//       const durationInSeconds = (checkOut - checkIn) / 1000;
+//       return durationInSeconds < 0 ? 0 : durationInSeconds;
+//     };
+
+//     const groupedData = data.reduce((acc, item) => {
+//       if (!acc[item.date]) {
+//         acc[item.date] = {
+//           date: item.date,
+//           checkIns: [],
+//           totalDurationInSeconds: 0,
+//           earliestCheckInTime: null,
+//           latestCheckOutTime: null,
+//           checkin_status: "Absent",
+//           attendance_status: "Absent",
+//           timeDifference: "0h 0m 0s",
+//           totalDuration: "0h 0m 0s",
+//         };
+//       }
+
+//       const checkInDateTime = new Date(`1970-01-01T${item.check_in_time}Z`);
+//       let checkin_status = "On Time";
+//       let timeDifferenceSeconds = 0;
+
+//       if (checkInDateTime < startDateTime) {
+//         checkin_status = "Early";
+//         timeDifferenceSeconds = Math.abs(
+//           (startDateTime - checkInDateTime) / 1000
+//         );
+//       } else if (checkInDateTime > endDateTime) {
+//         checkin_status = "Late";
+//         timeDifferenceSeconds = Math.abs(
+//           (checkInDateTime - endDateTime) / 1000
+//         );
+//       }
+
+//       const durationInSeconds = calculateDurationInSeconds(
+//         item.check_in_time,
+//         item.check_out_time
+//       );
+
+//       acc[item.date].checkIns.push({
+//         check_in_time: item.check_in_time,
+//         check_out_time: item.check_out_time || null,
+//         duration: formatDuration(durationInSeconds),
+//       });
+
+//       acc[item.date].totalDurationInSeconds += durationInSeconds;
+
+//       if (item.check_in_time) {
+//         acc[item.date].attendance_status = "Present";
+//         if (
+//           !acc[item.date].earliestCheckInTime ||
+//           item.check_in_time < acc[item.date].earliestCheckInTime
+//         ) {
+//           acc[item.date].earliestCheckInTime = item.check_in_time;
+//         }
+//       }
+
+//       if (item.check_out_time !== null) {
+//         if (
+//           !acc[item.date].latestCheckOutTime ||
+//           item.check_out_time > acc[item.date].latestCheckOutTime
+//         ) {
+//           acc[item.date].latestCheckOutTime = item.check_out_time;
+//         }
+//       } else {
+//         acc[item.date].latestCheckOutTime = null;
+//       }
+
+//       acc[item.date].checkin_status = checkin_status;
+//       acc[item.date].timeDifference = formatDuration(timeDifferenceSeconds);
+
+//       return acc;
+//     }, {});
+
+//     // Convert grouped data into an array
+//     const checkInDates = Object.values(groupedData).map((dateData) => ({
+//       ...dateData,
+//       totalDuration: formatDuration(dateData.totalDurationInSeconds),
+//     }));
+
+//     const employeeData = {
+//       id: data[0].id,
+//       name: data[0].name,
+//       mobile: data[0].mobile,
+//       email: data[0].email,
+//       designation: data[0].designation,
+//       employee_id: data[0].employee_id,
+//       image: data[0].image,
+//       checkInsByDate: checkInDates,
+//     };
+
+//     res.status(200).send({
+//       status: true,
+//       data: employeeData,
+//     });
+//   } catch (error) {
+//     res.status(500).send({ status: false, error: error.message });
+//   }
+// };
+
 exports.getEmployeeAttendance = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
@@ -208,8 +406,7 @@ exports.getEmployeeAttendance = async (req, res, next) => {
         .send({ status: false, message: "Employee not found" });
     }
 
-    const emp_id = employee.id;
-    const company_id = employee.company_id;
+    const { id: emp_id, company_id } = employee;
 
     if (!emp_id || !company_id) {
       return res.status(400).send({
@@ -218,46 +415,52 @@ exports.getEmployeeAttendance = async (req, res, next) => {
       });
     }
 
-    const query = `
-      SELECT 
-        e.id,
-        e.name,
-        e.mobile,
-        e.email,
-        e.designation,
-        e.employee_id,
-        CASE
-          WHEN e.image IS NOT NULL THEN CONCAT(?, e.image)
-          ELSE e.image
-        END AS image,
-        c.date,
-        c.check_in_time,
-        c.check_out_time
-      FROM employees e
-      LEFT JOIN check_in c ON e.id = c.emp_id AND e.company_id = c.company_id
-      WHERE e.id = ? AND e.company_id = ?
-      ORDER BY c.date, c.check_in_time
-    `;
+    const dateParam = req.query.date;
+    const targetDate = dateParam ? new Date(dateParam) : new Date();
+    const year = targetDate.getFullYear();
+    const month = targetDate.getMonth() + 1;
 
-    const values = [process.env.BASE_URL, emp_id, company_id];
+    const daysInMonth = new Date(year, month, 0).getDate();
+    const allDays = Array.from({ length: daysInMonth }, (_, i) => {
+      const day = String(i + 1).padStart(2, "0");
+      return `${year}-${String(month).padStart(2, "0")}-${day}`;
+    });
+
+    const query = `
+          SELECT 
+            e.id,
+            e.name,
+            e.mobile,
+            e.email,
+            e.designation,
+            e.employee_id,
+            CASE
+              WHEN e.image IS NOT NULL THEN CONCAT(?, e.image)
+              ELSE e.image
+            END AS image,
+            c.date,
+            c.check_in_time,
+            c.check_out_time
+          FROM employees e
+          LEFT JOIN check_in c ON e.id = c.emp_id AND e.company_id = c.company_id
+          WHERE e.id = ? AND e.company_id = ? AND MONTH(c.date) = ? AND YEAR(c.date) = ?
+          ORDER BY c.date, c.check_in_time
+        `;
+
+    const values = [process.env.BASE_URL, emp_id, company_id, month, year];
     const data = await sqlModel.customQuery(query, values);
 
     if (data.error) {
       return res.status(500).send(data);
     }
 
-    if (data.length === 0) {
-      return res.status(200).send({ status: false, message: "No data found" });
-    }
-
     const companyQuery = `
-      SELECT
-        check_in_time_start,
-        check_in_time_end
-      FROM company
-      WHERE id = ?
-    `;
-
+          SELECT
+            check_in_time_start,
+            check_in_time_end
+          FROM company
+          WHERE id = ?
+        `;
     const companyValues = [company_id];
     const companyData = await sqlModel.customQuery(companyQuery, companyValues);
 
@@ -268,7 +471,6 @@ exports.getEmployeeAttendance = async (req, res, next) => {
 
     const formatDuration = (totalSeconds) => {
       if (isNaN(totalSeconds) || totalSeconds < 0) {
-        console.error("Invalid totalSeconds value:", totalSeconds);
         return "0h 0m 0s";
       }
       const hours = Math.floor(totalSeconds / 3600);
@@ -279,7 +481,7 @@ exports.getEmployeeAttendance = async (req, res, next) => {
 
     const calculateDurationInSeconds = (checkInTime, checkOutTime) => {
       if (!checkInTime || !checkOutTime) {
-        return 0; // Return 0 if checkInTime or checkOutTime is null or undefined
+        return 0;
       }
       const checkIn = new Date(`1970-01-01T${checkInTime}Z`);
       const checkOut = new Date(`1970-01-01T${checkOutTime}Z`);
@@ -287,20 +489,23 @@ exports.getEmployeeAttendance = async (req, res, next) => {
       return durationInSeconds < 0 ? 0 : durationInSeconds;
     };
 
-    const groupedData = data.reduce((acc, item) => {
-      if (!acc[item.date]) {
-        acc[item.date] = {
-          date: item.date,
-          checkIns: [],
-          totalDurationInSeconds: 0,
-          earliestCheckInTime: null,
-          latestCheckOutTime: null,
-          checkin_status: "Absent",
-          attendance_status: "Absent",
-          timeDifference: "0h 0m 0s",
-          totalDuration: "0h 0m 0s",
-        };
-      }
+    const groupedData = allDays.reduce((acc, date) => {
+      acc[date] = {
+        date,
+        checkIns: [],
+        totalDurationInSeconds: 0,
+        earliestCheckInTime: null,
+        latestCheckOutTime: null,
+        checkin_status: "Absent",
+        attendance_status: "Absent",
+        timeDifference: "0h 0m 0s",
+        totalDuration: "0h 0m 0s",
+      };
+      return acc;
+    }, {});
+
+    data.forEach((item) => {
+      if (!groupedData[item.date]) return;
 
       const checkInDateTime = new Date(`1970-01-01T${item.check_in_time}Z`);
       let checkin_status = "On Time";
@@ -323,55 +528,54 @@ exports.getEmployeeAttendance = async (req, res, next) => {
         item.check_out_time
       );
 
-      acc[item.date].checkIns.push({
+      groupedData[item.date].checkIns.push({
         check_in_time: item.check_in_time,
         check_out_time: item.check_out_time || null,
         duration: formatDuration(durationInSeconds),
       });
 
-      acc[item.date].totalDurationInSeconds += durationInSeconds;
+      groupedData[item.date].totalDurationInSeconds += durationInSeconds;
 
       if (item.check_in_time) {
-        acc[item.date].attendance_status = "Present";
+        groupedData[item.date].attendance_status = "Present";
         if (
-          !acc[item.date].earliestCheckInTime ||
-          item.check_in_time < acc[item.date].earliestCheckInTime
+          !groupedData[item.date].earliestCheckInTime ||
+          item.check_in_time < groupedData[item.date].earliestCheckInTime
         ) {
-          acc[item.date].earliestCheckInTime = item.check_in_time;
+          groupedData[item.date].earliestCheckInTime = item.check_in_time;
         }
       }
 
       if (item.check_out_time !== null) {
         if (
-          !acc[item.date].latestCheckOutTime ||
-          item.check_out_time > acc[item.date].latestCheckOutTime
+          !groupedData[item.date].latestCheckOutTime ||
+          item.check_out_time > groupedData[item.date].latestCheckOutTime
         ) {
-          acc[item.date].latestCheckOutTime = item.check_out_time;
+          groupedData[item.date].latestCheckOutTime = item.check_out_time;
         }
       } else {
-        acc[item.date].latestCheckOutTime = null;
+        groupedData[item.date].latestCheckOutTime = null;
       }
 
-      acc[item.date].checkin_status = checkin_status;
-      acc[item.date].timeDifference = formatDuration(timeDifferenceSeconds);
+      groupedData[item.date].checkin_status = checkin_status;
+      groupedData[item.date].timeDifference = formatDuration(
+        timeDifferenceSeconds
+      );
+    });
 
-      return acc;
-    }, {});
-
-    // Convert grouped data into an array
     const checkInDates = Object.values(groupedData).map((dateData) => ({
       ...dateData,
       totalDuration: formatDuration(dateData.totalDurationInSeconds),
     }));
 
     const employeeData = {
-      id: data[0].id,
-      name: data[0].name,
-      mobile: data[0].mobile,
-      email: data[0].email,
-      designation: data[0].designation,
-      employee_id: data[0].employee_id,
-      image: data[0].image,
+      id: data[0]?.id,
+      name: data[0]?.name,
+      mobile: data[0]?.mobile,
+      email: data[0]?.email,
+      designation: data[0]?.designation,
+      employee_id: data[0]?.employee_id,
+      image: data[0]?.image,
       checkInsByDate: checkInDates,
     };
 
