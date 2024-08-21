d4e5dd5f2cce48bd90695bb5856d1099e1679769
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-08-21 16:04:40 +0530

    d

--- controllers/admin/EmployeeTrackController.js
+++ controllers/admin/EmployeeTrackController.js
@@ -171,75 +171,6 @@ exports.getCoordinates = async (req, res, next) => {
 //   }
 // };
 
-// exports.getCoordinatesv2 = async (req, res, next) => {
-//   try {
-//     const whereClause = {};
-
-//     for (const key in req.query) {
-//       if (req.query.hasOwnProperty(key)) {
-//         whereClause[key] = req.query[key];
-//       }
-//     }
-
-//     const query = `
-//       SELECT t.latitude, t.longitude, t.date, t.time,t.battery_status, subquery.cnt, subquery.min_time, subquery.max_time
-//       FROM (
-//           SELECT ROUND(latitude, 3) AS latitude, ROUND(longitude, 3) AS longitude, date,
-//                  MIN(time) AS min_time, MAX(time) AS max_time, COUNT(*) AS cnt, MIN(id) AS min_id
-//           FROM emp_tracking
-//           WHERE emp_id = ? AND date = ?
-//           GROUP BY ROUND(latitude, 3), ROUND(longitude, 3), date
-//       ) AS subquery
-//       JOIN emp_tracking AS t ON subquery.min_id = t.id
-//       ORDER BY subquery.min_time, subquery.max_time
-//     `;
-
-//     const emp_id = whereClause.emp_id || "";
-//     const date = whereClause.date || "";
-
-//     // Execute the custom query
-//     const data = await sqlModel.customQuery(query, [emp_id, date]);
-
-//     if (data.error) {
-//       return res.status(500).send(data);
-//     }
-
-//     if (data.length === 0) {
-//       return res.status(200).send({ status: false, message: "No data found" });
-//     }
-
-//     // Calculate the time difference for each group and sort by time
-//     const result = data.map((item) => {
-//       const minTime = new Date(`${item.date} ${item.min_time}`);
-//       const maxTime = new Date(`${item.date} ${item.max_time}`);
-//       const timeDifference = maxTime - minTime; // Time difference in milliseconds
-
-//       // Convert milliseconds to hours, minutes, seconds
-//       const hours = Math.floor(timeDifference / 3600000);
-//       const minutes = Math.floor((timeDifference % 3600000) / 60000);
-//       const seconds = Math.floor((timeDifference % 60000) / 1000);
-
-//       item.time_difference = `${hours}h ${minutes}m ${seconds}s`;
-
-//       // Assuming `item.image` is included in the data
-//       item.image = item.image ? `${process.env.BASE_URL}${item.image}` : "";
-
-//       return item;
-//     });
-
-//     // Sort the result by min_time to ensure proper sequence
-//     result.sort(
-//       (a, b) =>
-//         new Date(`${a.date} ${a.min_time}`) -
-//         new Date(`${b.date} ${b.min_time}`)
-//     );
-
-//     res.status(200).send({ status: true, data: result });
-//   } catch (error) {
-//     res.status(500).send({ status: false, error: error.message });
-//   }
-// };
-
 exports.getCoordinatesv2 = async (req, res, next) => {
   try {
     const whereClause = {};
@@ -257,10 +188,10 @@ exports.getCoordinatesv2 = async (req, res, next) => {
                  MIN(time) AS min_time, MAX(time) AS max_time, COUNT(*) AS cnt, MIN(id) AS min_id
           FROM emp_tracking
           WHERE emp_id = ? AND date = ?
-          GROUP BY ROUND(latitude, 3), ROUND(longitude, 3)
+          GROUP BY ROUND(latitude, 3), ROUND(longitude, 3), date
       ) AS subquery
       JOIN emp_tracking AS t ON subquery.min_id = t.id
-      ORDER BY t.date, t.time
+      ORDER BY subquery.min_time, subquery.max_time
     `;
 
     const emp_id = whereClause.emp_id || "";
@@ -277,42 +208,111 @@ exports.getCoordinatesv2 = async (req, res, next) => {
       return res.status(200).send({ status: false, message: "No data found" });
     }
 
-    // Process the data to calculate time differences between unique coordinates
-    let lastItem = null;
-    const result = [];
+    // Calculate the time difference for each group and sort by time
+    const result = data.map((item) => {
+      const minTime = new Date(`${item.date} ${item.min_time}`);
+      const maxTime = new Date(`${item.date} ${item.max_time}`);
+      const timeDifference = maxTime - minTime; // Time difference in milliseconds
 
-    for (const item of data) {
-      if (
-        lastItem &&
-        (lastItem.latitude !== item.latitude ||
-          lastItem.longitude !== item.longitude)
-      ) {
-        const minTime = new Date(`${lastItem.date}T${lastItem.max_time}.000Z`);
-        const maxTime = new Date(`${item.date}T${item.min_time}.000Z`);
-        const timeDifference = maxTime - minTime; // Time difference in milliseconds
+      // Convert milliseconds to hours, minutes, seconds
+      const hours = Math.floor(timeDifference / 3600000);
+      const minutes = Math.floor((timeDifference % 3600000) / 60000);
+      const seconds = Math.floor((timeDifference % 60000) / 1000);
 
-        // Convert milliseconds to hours, minutes, seconds
-        const hours = Math.floor(timeDifference / 3600000);
-        const minutes = Math.floor((timeDifference % 3600000) / 60000);
-        const seconds = Math.floor((timeDifference % 60000) / 1000);
+      item.time_difference = `${hours}h ${minutes}m ${seconds}s`;
 
-        lastItem.time_difference = `${hours}h ${minutes}m ${seconds}s`;
-        result.push(lastItem);
-      }
-      lastItem = item;
-    }
+      // Assuming `item.image` is included in the data
+      item.image = item.image ? `${process.env.BASE_URL}${item.image}` : "";
 
-    // Add the last item if it's unique
-    if (lastItem) {
-      lastItem.time_difference = "N/A"; // No time difference for the last entry
-      result.push(lastItem);
-    }
+      return item;
+    });
+
+    // Sort the result by min_time to ensure proper sequence
+    result.sort(
+      (a, b) =>
+        new Date(`${a.date} ${a.min_time}`) -
+        new Date(`${b.date} ${b.min_time}`)
+    );
 
     res.status(200).send({ status: true, data: result });
   } catch (error) {
     res.status(500).send({ status: false, error: error.message });
   }
 };
+
+// exports.getCoordinatesv2 = async (req, res, next) => {
+//   try {
+//     const whereClause = {};
+
+//     for (const key in req.query) {
+//       if (req.query.hasOwnProperty(key)) {
+//         whereClause[key] = req.query[key];
+//       }
+//     }
+
+//     const query = `
+//       SELECT t.latitude, t.longitude, t.date, t.time,t.battery_status, subquery.cnt, subquery.min_time, subquery.max_time
+//       FROM (
+//           SELECT ROUND(latitude, 3) AS latitude, ROUND(longitude, 3) AS longitude, date,
+//                  MIN(time) AS min_time, MAX(time) AS max_time, COUNT(*) AS cnt, MIN(id) AS min_id
+//           FROM emp_tracking
+//           WHERE emp_id = ? AND date = ?
+//           GROUP BY ROUND(latitude, 3), ROUND(longitude, 3)
+//       ) AS subquery
+//       JOIN emp_tracking AS t ON subquery.min_id = t.id
+//       ORDER BY t.date, t.time
+//     `;
+
+//     const emp_id = whereClause.emp_id || "";
+//     const date = whereClause.date || "";
+
+//     // Execute the custom query
+//     const data = await sqlModel.customQuery(query, [emp_id, date]);
+
+//     if (data.error) {
+//       return res.status(500).send(data);
+//     }
+
+//     if (data.length === 0) {
+//       return res.status(200).send({ status: false, message: "No data found" });
+//     }
+
+//     // Process the data to calculate time differences between unique coordinates
+//     let lastItem = null;
+//     const result = [];
+
+//     for (const item of data) {
+//       if (
+//         lastItem &&
+//         (lastItem.latitude !== item.latitude ||
+//           lastItem.longitude !== item.longitude)
+//       ) {
+//         const minTime = new Date(`${lastItem.date}T${lastItem.max_time}.000Z`);
+//         const maxTime = new Date(`${item.date}T${item.min_time}.000Z`);
+//         const timeDifference = maxTime - minTime; // Time difference in milliseconds
+
+//         // Convert milliseconds to hours, minutes, seconds
+//         const hours = Math.floor(timeDifference / 3600000);
+//         const minutes = Math.floor((timeDifference % 3600000) / 60000);
+//         const seconds = Math.floor((timeDifference % 60000) / 1000);
+
+//         lastItem.time_difference = `${hours}h ${minutes}m ${seconds}s`;
+//         result.push(lastItem);
+//       }
+//       lastItem = item;
+//     }
+
+//     // Add the last item if it's unique
+//     if (lastItem) {
+//       lastItem.time_difference = "N/A"; // No time difference for the last entry
+//       result.push(lastItem);
+//     }
+
+//     res.status(200).send({ status: true, data: result });
+//   } catch (error) {
+//     res.status(500).send({ status: false, error: error.message });
+//   }
+// };
 
 exports.getEmpLoginDetail = async (req, res, next) => {
   try {
