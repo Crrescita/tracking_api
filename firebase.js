c4439b968e53820c96ca897d619afd7991aa328f
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-09-07 20:31:27 +0530

    fb

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
 const base64ServiceAccountKey = process.env.GOOGLE_CLOUD_KEY;
 
 const serviceAccount = JSON.parse(
