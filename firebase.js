331a46c8422e9222c6302945d31604aeeb3e06ca
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-09-07 20:30:42 +0530

    firease

--- firebase.js
+++ firebase.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
+const base64ServiceAccountKey = process.env.GOOGLE_CLOUD_KEY;
+
+const serviceAccount = JSON.parse(
+  Buffer.from(base64ServiceAccountKey, "base64").toString("utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
