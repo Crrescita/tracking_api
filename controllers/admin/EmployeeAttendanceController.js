38477b28aaeacdcf298cd0522a646be39992f413
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2024-10-23 16:28:54 +0530

    order

--- controllers/admin/EmployeeAttendanceController.js
+++ controllers/admin/EmployeeAttendanceController.js
@@ -147,6 +147,7 @@ exports.getAttendance = async (req, res, next) => {
     LEFT JOIN emp_attendance a ON e.id = a.emp_id AND a.date = ?
     LEFT JOIN check_in c ON e.id = c.emp_id AND c.date = ? AND e.company_id = c.company_id
     WHERE e.company_id = ?
+    ORDER BY c.check_in_time ASC
 `;
 
     const values = [baseUrl, date, date, company_id];
