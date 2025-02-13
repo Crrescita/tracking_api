0146410a6ab66075b7c992c1acacef0dc3dcdacb
Sudhanshu Pal <sudhanshu.pal@firstfiddle.in>
2025-02-13 12:51:02 +0530

    d

--- controllers/frontend/MyRecordController.js
+++ controllers/frontend/MyRecordController.js
@@ -285,7 +285,7 @@ exports.getRecord = async (req, res, next) => {
       },
       {
         title: "Check-in Status",
-        icon: "https://telindia.s3.ap-south-1.amazonaws.com/icons/location.png",
+        icon: "https://trackingapi.crrescita.com/images/support/image2025-02-13T07-19-22.830Z-compressed_image_main.jpg",
         detail: checkin_status,
       },
       // {
